8c37baf95914cccf67aa397945a49d7718c0c569
Роман <zigzagforever@mail.ru>
2016-01-18 17:09:12 +0200

    //

--- OTHER/updateFirstRoom.sql
+++ OTHER/updateFirstRoom.sql
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 -- База данных: `test`
 --
 
--- --------------------------------------------------------
-
 --
 -- Структура таблицы `chat_room`
 --
@@ -33,8 +31,9 @@ CREATE TABLE IF NOT EXISTS `chat_room` (
   `author_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_fy1be4k30rsfiyj277sx585ev` (`author_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `chat_room_users`
@@ -49,31 +48,45 @@ CREATE TABLE IF NOT EXISTS `chat_room_users` (
 
 -- --------------------------------------------------------
 
+--
+-- Структура таблицы `chat_user`
+--
+
+CREATE TABLE IF NOT EXISTS `chat_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nick_name` varchar(50) DEFAULT NULL,
+  `intita_user_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
+
+--
+-- Структура таблицы `chat_user_message`
+--
+
+CREATE TABLE IF NOT EXISTS `chat_user_message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `body` varchar(64000) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `room_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_j1vvofqapsyianm36bc7biu6p` (`author_id`),
+  KEY `FK_o1i4vq6pnq4h9rcqa88khfulv` (`room_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
 
 --
--- Ограничения внешнего ключа таблицы `chat_room`
---
--- ALTER TABLE `chat_room`
-  -- ADD CONSTRAINT `FK_fy1be4k30rsfiyj277sx585ev` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`);
-
---
 -- Ограничения внешнего ключа таблицы `chat_room_users`
 --
--- ALTER TABLE `chat_room_users`
-  -- ADD CONSTRAINT `FK_8uci3ndco1sna4jf1t5u33lia` FOREIGN KEY (`rooms_from_users_id`) REFERENCES `chat_room` (`id`),
-  -- ADD CONSTRAINT `FK_9ryl67smn61vqw2j0wrl3lxtw` FOREIGN KEY (`users_id`) REFERENCES `user` (`id`);
-  
-  CREATE TABLE IF NOT EXISTS `user_message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `body` varchar(64000) DEFAULT NULL,
-  `author_id` int(11) DEFAULT NULL,
-  `room_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+ALTER TABLE `chat_room_users`
+  ADD CONSTRAINT `FK_8uci3ndco1sna4jf1t5u33lia` FOREIGN KEY (`rooms_from_users_id`) REFERENCES `chat_room` (`id`);
+
+--
+ALTER TABLE `chat_user_message`
+  ADD CONSTRAINT `FK_j1vvofqapsyianm36bc7biu6p` FOREIGN KEY (`author_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_o1i4vq6pnq4h9rcqa88khfulv` FOREIGN KEY (`room_id`) REFERENCES `chat_room` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
