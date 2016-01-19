be7f25db971a8a3bb9b4f8b904aa5d7c25796b8b
Самоенко Юрий <krdcl@mail.ru>
2016-01-19 15:05:29 +0200

    add chat_tenant table

---
+++ src/main/java/com/intita/wschat/repositories/ChatTenantRepository.java
@@ -0,0 +1,20 @@
+package com.intita.wschat.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+
+import com.intita.wschat.models.ChatTenant;
+import com.intita.wschat.models.User;
+
+public interface ChatTenantRepository extends CrudRepository<ChatTenant, Long> {
+	   Page<ChatTenant> findById(Long id, Pageable pageable);
+	   ChatTenant findById(Long id);
+	   Page<ChatTenant> findAll(Pageable pageable);
+	 /*  ChatTenant findOneByIntitaUser(User user);
+	   List<ChatTenant> findFirst10ByIdNotIn(List<Long> users);
+	 //  List<User> findFirst5ByLoginAndByPassword( String users, String login);
+	   List<ChatTenant> findFirst5ByNickNameNotInAndNickNameLike( List<String> users, String login);*/
+	 }
