993940ecedab7112baf6ff64df8407b4a43313e4
Nicolas Haiduchok <nico13051995@gmail.com>
2016-03-26 11:00:16 +0200

    //add DB files

---
+++ OTHER/consultation/chat_consultation_ratings.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 26 2016 г., 09:58
+-- Версия сервера: 5.6.17
+-- Версия PHP: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `qa_intita`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `chat_consultation_ratings`
+--
+
+CREATE TABLE IF NOT EXISTS `chat_consultation_ratings` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+
+--
+-- Дамп данных таблицы `chat_consultation_ratings`
+--
+
+INSERT INTO `chat_consultation_ratings` (`id`, `name`) VALUES
+(1, 'global'),
+(3, 'smart');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
