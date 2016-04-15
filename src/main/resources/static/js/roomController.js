00f4f887e1fcc7d308322bfd956f773f0260fa2b
Nicolas Haiduchok <nico13051995@gmail.com>
2016-04-15 23:30:26 +0300

    //

--- src/main/resources/static/js/roomController.js
+++ src/main/resources/static/js/roomController.js
@@ -65,6 +65,31 @@ springChatControllers.controller('DialogsRouteController',['$q','$rootScope','$s
 
 		chatControllerScope.currentRoom = {roomId: ''} ;
 	}
+
+	$('.selected-items-box').bind('click', function(e){
+  e.stopPropagation();
+  $('.multiple-select-wrapper .list').toggle('slideDown');
+});
+
+$('.multiple-select-wrapper .list').bind('click', function(e){
+	e.stopPropagation();
+});
+
+$(document).bind('click', function(){
+	$('.multiple-select-wrapper .list').slideUp();	
+});
+  
+$scope.Airlines = [
+  {selected: false, name:'SWISS', img:'http://s9.postimage.org/d9t33we17/Swiss.png'},
+	{selected: false, name:'UNITED', img:'http://s9.postimage.org/ykqn85w5n/United.png'},
+	{selected: false, name:'KLM', img:'http://s9.postimage.org/p7unhshsb/Klm.png'},
+	{selected: false, name:'EL AL', img:'http://s18.postimage.org/oi8ndntud/image.gif'},
+	{selected: false, name:'Ethiopian', img:'http://s9.postimage.org/hqlg2ks97/image.gif'}
+];
+
+$scope.getSelectedItemsOnly = function(item){
+	return item.selected;
+};
 	
 	/*
 	 * 
