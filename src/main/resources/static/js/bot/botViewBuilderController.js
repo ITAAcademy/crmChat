6c70397ef32e2b2975032a375429d53cad40bb99
Nicolas Haiduchok <nico13051995@mail.com>
2016-05-17 18:50:31 +0300

    //

--- src/main/resources/static/js/bot/botViewBuilderController.js
+++ src/main/resources/static/js/bot/botViewBuilderController.js
@@ -21,4 +21,20 @@ springChatControllers.controller('ChatBotViewBuilderController', ['$routeParams'
         name: 'Tabs'
     };
 
+     $scope.models = {
+        selected: null,
+        lists: {"A": [], "B": []}
+    };
+
+    // Generate initial model
+    for (var i = 1; i <= 3; ++i) {
+        $scope.models.lists.A.push({label: "Item A" + i});
+        $scope.models.lists.B.push({label: "Item B" + i});
+    }
+
+    // Model to JSON for demo purpose
+    $scope.$watch('models', function(model) {
+        $scope.modelAsJson = angular.toJson(model, true);
+    }, true);
+
 }]);
