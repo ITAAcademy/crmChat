6d3cb59d8dbc098f0c2306187a2d0759b3e35d4b
Samoenko Yura <krdcl@mail.ru>
2016-05-27 19:25:20 +0300

    vertical / horizontal list

--- src/main/resources/static/js/bot/botElementsModule.js
+++ src/main/resources/static/js/bot/botElementsModule.js
@@ -71,14 +71,14 @@ var BOT_ELEMENTS_MODULE = function() {
             /*if((scope.elementsListForLink.length - 1) %2 == 0)
                 leftTooltip = "";*/
             var addedPropertyFinal = this.addedProperty + ' dnd-placeholder-body = "' + this.type + '" dnd-dragover="$root.dragoverCallback(event, index, external, type, $root.this)" dnd-drop="$root.dropCallback(event, index, item, external, type, $root.this)" dnd-list="$root.this.childrens"';
-            var addedHeaderFinal = '<li  ' + 'tooltip-placement="{1}" tooltip-trigger="mouseenter" uib-tooltip="{0}"'.format(this.type, leftTooltip) + 'class = "render-element" dnd-dragstart = "$root.dragStart($root.this)" dnd-draggable="$root.this" dnd-effect-allowed="move" dnd-selected="$root.models.selected = $root.this">';
+            var addedHeaderFinal = '<li  class="custom-grid-li"' + 'tooltip-placement="{1}" tooltip-trigger="mouseenter" uib-tooltip="{0}"'.format(this.type, leftTooltip) + 'class = "render-element" dnd-dragstart = "$root.dragStart($root.this)" dnd-draggable="$root.this" dnd-effect-allowed="move" dnd-selected="$root.models.selected = $root.this">';
             var addedFooterFinal = '</li>';
             if (ignoreAddedProperties) {
                 addedClassesFinal = addedHeaderFinal = addedFooterFinal = addedPropertyFinal = "";
             }
             var template;
             if (!ignoreAddedProperties)
-                template = addedHeaderFinal + '<div class="wrap"><ul {0} = " " {1} {2}>{3}</ul></div>'.format(this.type, addedClassesFinal, propertiesStr + " " + addedPropertyFinal, childrensStr) + addedFooterFinal;
+                template = addedHeaderFinal + '<div class="wrap"><ul class="custom-ul" {0} = " " {1} {2}>{3}</ul></div>'.format(this.type, addedClassesFinal, propertiesStr + " " + addedPropertyFinal, childrensStr) + addedFooterFinal;
             else
                 template = addedHeaderFinal + '<{0} {1} {2}>{3}</{0}>'.format(this.type, addedClassesFinal, propertiesStr + " " + addedPropertyFinal, childrensStr) + addedFooterFinal;
 
