c5e73de88de44d86b021b0a2e87b167a6b92cb84
Samoenko Yura <krdcl@mail.ru>
2016-06-17 15:34:14 +0300

    //

--- src/main/java/com/intita/wschat/models/BotCategory.java
+++ src/main/java/com/intita/wschat/models/BotCategory.java
@@ -1,19 +1,25 @@
 package com.intita.wschat.models;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-public class BotCategory {
+public class BotCategory implements java.io.Serializable {
 	@Id
 	@GeneratedValue
 	private Long id;
@@ -23,7 +29,7 @@ public class BotCategory {
 	@OneToOne
 	@JsonIgnore
 	private BotDialogItem mainElement;
-	
+
 	@JsonIgnore
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "category")
 	List<BotDialogItem> elements = new ArrayList<BotDialogItem>();
@@ -67,4 +73,32 @@ public class BotCategory {
 	public void setMainElement(BotDialogItem mainElement) {
 		this.mainElement = mainElement;
 	}
+
+	@ManyToMany(mappedBy = "botCategories", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	Set<ChatTenant> chatUsers  = new HashSet<>();
+/*
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "user_tenant_categories", 
+			joinColumns = @JoinColumn
+					(
+						name = "bot_category_id", 
+						referencedColumnName = "id"
+					),
+			inverseJoinColumns = @JoinColumn
+					(
+						name = "user_tenant_id",
+						referencedColumnName = "id"
+					)
+			)*/
+	public Set<ChatTenant> getChatUsers() {
+		return chatUsers;
+	}
+	public void setChatUsers(Set<ChatTenant> chatUsers) {
+		this.chatUsers = chatUsers;
+	}
+	
+	
+
+
+
 }
