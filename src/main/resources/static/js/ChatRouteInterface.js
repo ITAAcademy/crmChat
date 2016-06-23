b8243672299f7d1f71523cd36468597f2db94e89
Nicolas Haiduchok <nico13051995@mail.com>
2016-06-23 20:08:04 +0300

    //

--- src/main/resources/static/js/ChatRouteInterface.js
+++ src/main/resources/static/js/ChatRouteInterface.js
@@ -789,15 +789,10 @@ springChatControllers.controller('ChatRouteInterface', ['$route', '$routeParams'
             totalSize += files[i].size;
         }
         if (totalSize > MAX_UPLOAD_FILE_SIZE_BYTES) {
-            alert(fileUploadLocal.fileSizeOverflowLimit + ":" + Math.round(totalSize / 1024) + "/" + MAX_UPLOAD_FILE_SIZE_BYTES / 1024 + "Kb");
+            var noteStr = fileUploadLocal.fileSizeOverflowLimit + ":" + Math.round(totalSize / 1024) + "/" + MAX_UPLOAD_FILE_SIZE_BYTES / 1024 + "Kb";
+            toaster.pop('error', "Failed", noteStr, 5000);
             return;
         }
-
-          if (totalSize>MAX_UPLOAD_FILE_SIZE_BYTES){
-                var noteStr = fileUploadLocal.fileSizeOverflowLimit+":"+Math.round(totalSize/1024)+"/"+ MAX_UPLOAD_FILE_SIZE_BYTES/1024 +"Kb";
-          toaster.pop('error', "Failed", noteStr, 0);
-                return;
-            }
 
         //Upload the image
         //(Uploader is a service in my application, you will need to create your own)
@@ -1361,22 +1356,25 @@ springChatControllers.controller('ChatRouteInterface', ['$route', '$routeParams'
     $scope.$$postDigest(function() {
         var nice = $(".scroll").niceScroll();
 
-        var fileInput = $("#myfile").fileinput({ language: "ua", maxFileSize: MAX_UPLOAD_FILE_SIZE_BYTES/1000,minFileSize: 1, showCaption: false, initialPreviewShowDelete: true, browseLabel: "", browseClass: " btn btn-primary load-btn", uploadExtraData: { kvId: '10' } });
+        var fileInput = $("#myfile").fileinput({ language: "ua", maxFileSize: MAX_UPLOAD_FILE_SIZE_BYTES / 1000, minFileSize: 1, showCaption: false, initialPreviewShowDelete: true, browseLabel: "", browseClass: " btn btn-primary load-btn", uploadExtraData: { kvId: '10' } });
         $('#myfile').on('change', function(event, numFiles, label) {
             var totalFilesLength = 0;
-    for (var i = 0; i < this.files.length; i++){
-    totalFilesLength += this.files[i].size;
-    }
-    if (totalFilesLength > MAX_UPLOAD_FILE_SIZE_BYTES) {
-          $('#myfile').fileinput('lock');
-          var noteStr = fileUploadLocal.fileSizeOverflowLimit+":"+Math.round(totalFilesLength/1024)+"/"+ MAX_UPLOAD_FILE_SIZE_BYTES/1024 +"Kb";
-          //toaster.pop('note', "Created", noteStr, 0);
-          alert(noteStr);
-    }
-    });
-          $('#myfile').on('fileclear', function(event, numFiles, label) {
-             $('#myfile').fileinput('unlock');
-    });
+            for (var i = 0; i < this.files.length; i++) {
+                totalFilesLength += this.files[i].size;
+            }
+            if (totalFilesLength > MAX_UPLOAD_FILE_SIZE_BYTES) {
+                $('#myfile').fileinput('lock');
+                var noteStr = fileUploadLocal.fileSizeOverflowLimit + ":" + Math.round(totalFilesLength / 1024) + "/" + MAX_UPLOAD_FILE_SIZE_BYTES / 1024 + "Kb";
+                $scope.$apply(function() {
+                    toaster.pop('error', "Failed", noteStr, 5000);
+                });
+
+                //  alert(noteStr);
+            }
+        });
+        $('#myfile').on('fileclear', function(event, numFiles, label) {
+            $('#myfile').fileinput('unlock');
+        });
 
     });
 
