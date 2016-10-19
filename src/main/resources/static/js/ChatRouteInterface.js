a1f80f54860b802a7bfd4e7b802f955f8bcda272
Nicolas Haiduchok <nico13051995@mail.com>
2016-10-19 14:43:21 +0300

    //restore trainer

--- src/main/resources/static/js/ChatRouteInterface.js
+++ src/main/resources/static/js/ChatRouteInterface.js
@@ -1328,7 +1328,7 @@ springChatControllers.controller('ChatRouteInterface', ['$route', '$routeParams'
 
     $scope.checkUserAdditionPermission = function() {
         if (typeof chatControllerScope.currentRoom === "undefined") return false;
-        var resultOfChecking = chatControllerScope.currentRoom.active && ($scope.roomType != 1) && (chatControllerScope.chatUserId == chatControllerScope.currentRoom.roomAuthorId) && chatControllerScope.isMyRoom && $rootScope.authorize;
+        var resultOfChecking = chatControllerScope.currentRoom.active /*&& ($scope.roomType != 1)*/ && (chatControllerScope.chatUserId == chatControllerScope.currentRoom.roomAuthorId) && chatControllerScope.isMyRoom && $rootScope.authorize;
         return resultOfChecking;
     }
 
