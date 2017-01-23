2c3990b9422406fce8bfa35f087440537f27d168
roman zinchuk <zigzagforever@mail.ru>
2017-01-23 18:14:48 +0200

    gotoroom by route before inited fixed

--- src/main/resources/static/js/mainController.js
+++ src/main/resources/static/js/mainController.js
@@ -9,6 +9,26 @@ springChatControllers.config(function($routeProvider) {
         templateUrl: "builderTemplateJSTemp.html",
         controller: "ChatBotFormBuilderController"
     });
+    $routeProvider.when("/dialog_view/:roomId/", {
+        resolve: {
+            load: function($route, RoomsFactory,ChannelFactory, $routeParams) {
+                if (!ChannelFactory.getIsInited()) return;
+                RoomsFactory.goToRoom($route.current.params.roomId);
+            }
+        }
+    });
+    $routeProvider.when("/access_deny", {
+        templateUrl: "accessDeny.html",
+        controller: "AccessDeny"
+    });
+    $routeProvider.when("/private_dialog_view/:chatUserId", {
+        resolve: {
+            load: function($route, RoomsFactory, $routeParams) {
+                RoomsFactory.goToPrivateDialog($route.current.params.chatUserId);
+            }
+        }
+    });
+
     $routeProvider.otherwise({ redirectTo: '/' });
     console.log("scope test");
 
