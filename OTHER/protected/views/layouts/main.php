e103a951a16dcf4049115d89c7b6b91d5416db65
DESKTOP-OFCB56D\nico <nico13051995@gmail.com>
2017-02-07 23:04:59 +0200

    //many + many

--- OTHER/protected/views/layouts/main.php
+++ OTHER/protected/views/layouts/main.php
@@ -305,15 +305,23 @@ $header = new Header();
 <!-- Placeholder for old browser -->
 <script src="<?php echo StaticFilesHelper::fullPathTo('js', 'placeholder.min.js'); ?>"></script>
 <!-- Placeholder for old browser -->
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script type="text/javascript">
+    /*
+ * jQuery resize event - v1.1 - 3/14/2010
+ * http://benalman.com/projects/jquery-resize-plugin/
+ * 
+ * Copyright (c) 2010 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+(function($,h,c){var a=$([]),e=$.resize=$.extend($.resize,{}),i,k="setTimeout",j="resize",d=j+"-special-event",b="delay",f="throttleWindow";e[b]=250;e[f]=true;$.event.special[j]={setup:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.add(l);$.data(this,d,{w:l.width(),h:l.height()});if(a.length===1){g()}},teardown:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.not(l);l.removeData(d);if(!a.length){clearTimeout(i)}},add:function(l){if(!e[f]&&this[k]){return false}var n;function m(s,o,p){var q=$(this),r=$.data(this,d);r.w=o!==c?o:q.width();r.h=p!==c?p:q.height();n.apply(this,arguments)}if($.isFunction(l)){n=l;return m}else{n=l.handler;l.handler=m}}};function g(){i=h[k](function(){a.each(function(){var n=$(this),m=n.width(),l=n.height(),o=$.data(this,d);if(m!==o.w||l!==o.h){n.trigger(j,[o.w=m,o.h=l])}});g()},e[b])}})(jQuery,this);
+</script>
+
+   <script type="text/javascript">
 
 
-<script src="https://localhost:8080/crmChat/js/ITA.js"></script>
 
-<!-- Placeholder for old browser
-    <script type="text/javascript">
-
-    var html = "<link href=\"https:\/\/fonts.googleapis.com\/icon?family=Material+Icons\" rel=\"stylesheet\"\/><style type=\"text\/css\">.dnd-container{position:fixed;z-index:99999999;top:0;left:0;width:100%;height:100%;transform:none;pointer-events:none}.dnd-container *{box-sizing:border-box}.dnd-container .chat{width:100%;height:100%;max-height:600px;max-width:400px;top:calc(100% - 600px);left:calc(100% - 400px);bottom:0;right:0;position:absolute;transform:none;pointer-events:all;-webkit-transition:all .5s ease;-moz-transition:all .5s ease;-o-transition:all .5s ease;transition:all .5s ease;-webkit-transition-property:top,bottom,left,right;transition-property:top,bottom,left,right;-webkit-transform:translateZ(0);-moz-transform:translateZ(0);-ms-transform:translateZ(0);-o-transform:translateZ(0);transform:translateZ(0);margin:0;height:600px!important;width:400px!important;max-height:100%}.dnd-container .chat.normal{-webkit-transition-property:all;transition-property:all}.dnd-container .chat.mini{max-height:65px;max-width:300px}.dnd-container .chat.full{max-height:100%;max-width:950px;height:100%!important;width:100%!important;right:0!important;bottom:0!important;top:0!important;left:0!important;margin:auto;-webkit-transition-property:none;transition-property:none}.dnd-container .chat.disable-animation{-webkit-transition:initial;-moz-transition:initial;-o-transition:initial;transition:initial}.dnd-container .chat .logo{background-color:white;background-image:url(https:\/\/qa.intita.com\/images\/mainpage\/Logo_small.png);background-repeat:no-repeat;background-size:contain;width:110px;height:30px;position:absolute;top:18px;left:25px;max-width:0}.dnd-container .chat.mini .logo{max-width:100%}.window_panel>*{display:inline-block;width:20px;height:35px;color:#fff;line-height:35px}.window_panel{position:absolute;top:15px;right:10px;width:60px;height:35px;background:0;color:#fff;text-align:center}.handle{position:absolute;top:15px;left:50px;width:calc(100% - 120px);height:35px;background:0;color:#fff;cursor:move}.dnd-container .chat.mini .handle{cursor:pointer}.dnd-container .chat.full .handle{pointer-events:none}.dnd-container .chat.mini #minimize_btn{display:none}.dnd-container .chat.full .window_panel{top:15px;right:10px}.material-icons{-webkit-transition:color .25s ease-in-out;-moz-transition:color .25s ease-in-out;-o-transition:color .25s ease-in-out;transition:color .25s ease-in-out}.material-icons:hover{cursor:pointer;color:#a3c6e2}.disable-mouse{pointer-events:none}@media only screen and (min-width:900px){.dnd-container .chat.full .window_panel{top:15px;right:25px}}<\/style><div ng-controller=\"chat-controller as main\" class=\"dnd-container\"><div ng-show=\"init\" class=\"draggable chat mini ng-class:{mini: state==1, full: state==2}\" ng-click=\"\"><iframe style=\"width: 100%;height: 100%;border: none;\" src=\"https:\/\/localhost:8080\/crmChat\"><\/iframe><div class=\"window_panel ignore\" style=\"\"><div id=\"minimize_btn\" class=\"material-icons\" ng-click=\"minimizete()\">indeterminate_check_box<\/div><div id=\"fullscreen_btn\" class=\"material-icons\" ng-click=\"fullScreen()\">web_asset<\/div><\/div><div class=\"handle\" ng-mouseup=\"minimizeteMin()\" style=\"\"><\/div><\/div><\/div><\/body><\/html>"
-    $("body").append(html);
     var app = angular.module('mainApp', []);
 
     app.controller('chat-controller', function($scope) {
@@ -401,9 +409,10 @@ $header = new Header();
 
         }
 
-        function resizeFunc(e) {
-            var elem = $(".dnd-container");
+    var elem = $(".dnd-container");
             var res_elem = $('.draggable');
+        function resizeFunc(e) {
+        
             var offset = res_elem.position();
             if (elem.height() < offset.top + res_elem.height() || offset.top < 0) {
                 res_elem.css({ top: (elem.height() - res_elem.height()) + 'px' });
@@ -411,16 +420,21 @@ $header = new Header();
             if (elem.width() < offset.left + res_elem.width() || offset.left < 0) {
                 res_elem.css({ left: (elem.width() - res_elem.width()) + 'px' });
             }
-            localStorage.setItem("chatX", res_elem.css("left"));
-            localStorage.setItem("chatY", res_elem.css("top"));
+            if($scope.state==1)
+            reinitElemPos();
+            //localStorage.setItem("chatX", res_elem.css("left"));
+            //localStorage.setItem("chatY", res_elem.css("top"));
         }
         $(window).resize(resizeFunc);
+
+      
+
 
 
         //  $(document).ready(resizeFunc);
 
-        $(document).ready(function() {
-            $scope.state = 0;
+        var reinitElemPos = function(){
+                        var elem = $(".dnd-container");
             var res_elem = $('.draggable');
             var x = localStorage.getItem("chatX");
             var y = localStorage.getItem("chatY");
@@ -429,12 +443,19 @@ $header = new Header();
                 res_elem.css({ top: (elem.height() - 600) + 'px' });
                 res_elem.css({ left: (elem.width() - 400) + 'px' });
             } else {
-                res_elem.css({ top: y });
                 res_elem.css({ left: x });
+                res_elem.css({ top: y });
             }
+        }
 
+        $(document).ready(function() {
+            $scope.state = 0;
+            var elem = $(".dnd-container");
+            var res_elem = $('.draggable');
+
+            
             $scope.$apply(function() {
-
+            $('.draggable').bind( 'resize', resizeFunc);
                 $scope.$watch('state', function() {
                     localStorage.setItem("chatState", $scope.state);
                     var res_elem = $('.draggable');
@@ -446,6 +467,8 @@ $header = new Header();
                             res_elem.addClass("normal");
                         }, 600);
                     }
+                    if($scope.state == 1)
+                        reinitElemPos();
                 })
 
                 if (localStorage.getItem("chatState") == undefined)
@@ -460,7 +483,6 @@ $header = new Header();
                     }
                 }
                 $(".chat").draggable({ /*handle: ".handle"*/ cancel: ".ignore", containment: "parent", start: $scope.dragstart, stop: $scope.dragend });
-
                 $scope.init = true;
             });
 
@@ -468,9 +490,9 @@ $header = new Header();
     });
 
 </script>
--->
+
 <!-- http://www.freeformatter.com/javascript-escape.html#ad-output $('body').append();-->
-<!--
+<
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
  <style type="text/css">
  .dnd-container{
@@ -561,8 +583,8 @@ transform: translateZ(0);
  .window_panel{
     position:absolute;top: 15px;right: 10px;width: 60px;height: 35px;background: none;color:#fff; text-align: center;
  }
- .handle{
-    position:absolute;top: 15px;left: 50px;width: calc(100% - 120px);height: 35px;background: none;color:#fff;cursor: move;
+ .dnd-container .chat .handle{
+    position:absolute;top: 15px;left: 50px;width: calc(100% - 160px);height: 35px;background: none;color:#fff;cursor: move;
  }
  .dnd-container .chat.mini .handle{
     cursor: pointer;
@@ -598,7 +620,9 @@ transform: translateZ(0);
 }
 }
   </style>
+<script type="text/javascript">
 
+</script>
 <div ng-controller="chat-controller as main" class="dnd-container">
             <div ng-show="init" class="draggable chat mini ng-class:{mini: state==1, full: state==2}" ng-click="" >
             <iframe style="width: 100%;height: 100%;border: none;" src="https://localhost:8080/crmChat"></iframe>
@@ -611,6 +635,6 @@ transform: translateZ(0);
 
             </div>
 </div>
--->
+
 </body>
 </html>
