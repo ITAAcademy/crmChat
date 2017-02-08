46c078cb4e9781ff766462a7604febaed34a2739
roman zinchuk <zigzagforever@mail.ru>
2017-02-08 15:58:47 +0200

    gulp improvements: added plumber for errors ignoring and new files watch

--- src/main/gulpfile.js
+++ src/main/gulpfile.js
@@ -12,6 +12,8 @@ const babel = require('gulp-babel');
 const autoprefixer = require('gulp-autoprefixer');
 const debug = require('gulp-debug');
 const path = require('path');
+const watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
 
 var gulpIf = require('gulp-if');
 //var ignorableJs = ["ngDialog.min.js"];//js which not to be minified
@@ -22,6 +24,7 @@ var uglifyOptions =  {
 
 gulp.task('scripts',function(){
     return gulp.src('source_resources/js/**/*.js',{since:gulp.lastRun('scripts')})
+        .pipe(plumber())
         .pipe(babel({presets:['es2015']}))
         .pipe(debug({title: 'babel:'}))
       /*  .pipe(uglify(uglifyOptions))*/
@@ -45,9 +48,12 @@ gulp.task('templates',function(){
 
 gulp.task('build',gulp.series('styles','assets','templates:static','templates','scripts','lib'));
 gutil.log('Start watching!');
-gulp.watch('source_resources/css/*.{css,sass}',gulp.series('styles'));
-gulp.watch('source_resources/images/**/*.*',gulp.series('assets'));
-gulp.watch('source_resources/static_templates/*.*',gulp.series('templates:static'));
-gulp.watch('source_resources/templates/*.*',gulp.series('templates'));
-gulp.watch('source_resources/js/**/*.*',gulp.series('scripts'));
-gulp.watch('source_resources/lib/*.*',gulp.series('lib'));
+gulp.task('watch',function(){
+    watch('source_resources/css/*.{css,sass}',gulp.series('styles'));
+    gulp.watch('source_resources/images/**/*.*',gulp.series('assets'));
+    gulp.watch('source_resources/static_templates/*.*',gulp.series('templates:static'));
+    gulp.watch('source_resources/templates/*.*',gulp.series('templates'));
+    gulp.watch('source_resources/js/**/*.*',gulp.series('scripts'));
+    gulp.watch('source_resources/lib/*.*',gulp.series('lib'));
+});
+gulp.task('dev',gulp.series('build','watch'));
