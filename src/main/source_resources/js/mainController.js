ea9c903baf36c70f9cbed054c8eb8407208aa99f
roman zinchuk <zigzagforever@mail.ru>
2017-04-10 21:46:40 +0300

    linkifying

--- src/main/source_resources/js/mainController.js
+++ src/main/source_resources/js/mainController.js
@@ -794,6 +794,52 @@ var chatController = springChatControllers.controller('ChatController', ['$sce',
             sel.addRange(range); 
             console.log("Inserted:"+text);       
         }
+function pasteHtmlAtCaret(html, selectPastedContent) {
+    var sel, range;
+    if (window.getSelection) {
+        // IE9 and non-IE
+        sel = window.getSelection();
+        if (sel.getRangeAt && sel.rangeCount) {
+            range = sel.getRangeAt(0);
+            range.deleteContents();
+
+            // Range.createContextualFragment() would be useful here but is
+            // only relatively recently standardized and is not supported in
+            // some browsers (IE9, for one)
+            var el = document.createElement("div");
+            el.innerHTML = html;
+            var frag = document.createDocumentFragment(), node, lastNode;
+            while ( (node = el.firstChild) ) {
+                lastNode = frag.appendChild(node);
+            }
+            var firstNode = frag.firstChild;
+            range.insertNode(frag);
+
+            // Preserve the selection
+            if (lastNode) {
+                range = range.cloneRange();
+                range.setStartAfter(lastNode);
+                if (selectPastedContent) {
+                    range.setStartBefore(firstNode);
+                } else {
+                    range.collapse(true);
+                }
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+        }
+    } else if ( (sel = document.selection) && sel.type != "Control") {
+        // IE < 9
+        var originalRange = sel.createRange();
+        originalRange.collapse(true);
+        sel.createRange().pasteHTML(html);
+        if (selectPastedContent) {
+            range = sel.createRange();
+            range.setEndPoint("StartToStart", originalRange);
+            range.select();
+        }
+    }
+}
 
         messageAreaResizer();
 
@@ -806,7 +852,9 @@ var chatController = springChatControllers.controller('ChatController', ['$sce',
     e.preventDefault();
     var text = (e.originalEvent || e).clipboardData.getData('text/plain');
     var escapedHtml = htmlEscape(text);
-    insertTextAtCursor(escapedHtml);
+
+    var linkified = linkifyStr(escapedHtml, {});
+    pasteHtmlAtCaret(linkified);
     //$scope.newMessage = text;
 
 });
