903a3994f69d9b78e3664d8ebc556a739f8d8926
nico13051995 <nico13051995@gmail.com>
2017-10-10 23:22:04 +0300

    //some fix

--- src/main/java/com/intita/wschat/models/UserMessage.java
+++ src/main/java/com/intita/wschat/models/UserMessage.java
@@ -25,152 +25,152 @@ import jsonview.Views;
 
 @Entity(name = "chat_user_message")
 public class UserMessage implements Serializable,Comparable<UserMessage>  {
-	
-	public UserMessage(){
-		this.date= new Date();
-	}
-	public UserMessage(ChatUser author, Room room, UserMessageDTO userMessage){
-	this.author = author;
-	this.room = room;
-	this.body = userMessage.getBody();
-	this.date= new Date();
-	this.setAttachedFiles(userMessage.getAttachedFiles());
-	}
-	public UserMessage(ChatUser author, Room room, String body){	
-		this.author = author;
-		this.room = room;
-		this.body = body;
-		this.date= new Date();
-		}
 
-		public static UserMessage forId(Long id){
-		UserMessage message = new UserMessage();
-		message.setId(id);
-		return message;
-		}
-	
-	@Id
-	@GeneratedValue
-	@JsonView(Views.Public.class)
-	private Long id;
-	
-	//@NotBlank
-	@ManyToOne( fetch = FetchType.LAZY )
-	@NotNull
-	@JsonManagedReference
-	@JsonView(Views.Public.class)
-	@NotFound(action=NotFoundAction.IGNORE)
-	private ChatUser author;
-	
-	@ManyToOne(  fetch = FetchType.LAZY )
-	@NotFound(action=NotFoundAction.IGNORE)
-	private Room room;
-	
-	@Size(max=64000)
-	@Column
-	@JsonView(Views.Public.class)
-	private String body;
+  public UserMessage(){
+    this.date= new Date();
+  }
+  public UserMessage(ChatUser author, Room room, UserMessageDTO userMessage){
+    this.author = author;
+    this.room = room;
+    this.body = userMessage.getBody();
+    this.date= new Date();
+    this.setAttachedFiles(userMessage.getAttachedFiles());
+  }
+  public UserMessage(ChatUser author, Room room, String body){	
+    this.author = author;
+    this.room = room;
+    this.body = body;
+    this.date= new Date();
+  }
 
-	public boolean isActive() {
-		return active;
-	}
+  public static UserMessage forId(Long id){
+    UserMessage message = new UserMessage();
+    message.setId(id);
+    return message;
+  }
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
+  @Id
+  @GeneratedValue
+  @JsonView(Views.Public.class)
+  private Long id;
 
-	public String getAttachedFilesJson() {
-		return attachedFilesJson;
-	}
+  //@NotBlank
+  @ManyToOne( fetch = FetchType.LAZY )
+  @NotNull
+  @JsonManagedReference
+  @JsonView(Views.Public.class)
+  @NotFound(action=NotFoundAction.IGNORE)
+  private ChatUser author;
 
-	public void setAttachedFilesJson(String attachedFilesJson) {
-		this.attachedFilesJson = attachedFilesJson;
-	}
+  @ManyToOne(  fetch = FetchType.LAZY )
+  @NotFound(action=NotFoundAction.IGNORE)
+  private Room room;
 
-	@JsonIgnore
-	@Column(nullable = false)
-	@ColumnDefault("1")
-	private boolean active = true;
+  @Size(max=64000)
+  @Column
+  @JsonView(Views.Public.class)
+  private String body;
 
-	@JsonView(Views.Public.class)
-	@Lob
-	private String attachedFilesJson;
+  public boolean isActive() {
+    return active;
+  }
 
-	
-	@Column
-	@JsonView(Views.Public.class)
-	private Date date;
+  public void setActive(boolean active) {
+    this.active = active;
+  }
 
-	public Date getUpdateat() {
-		return updateat;
-	}
+  public String getAttachedFilesJson() {
+    return attachedFilesJson;
+  }
 
-	public void setUpdateat(Date updateat) {
-		this.updateat = updateat;
-	}
+  public void setAttachedFilesJson(String attachedFilesJson) {
+    this.attachedFilesJson = attachedFilesJson;
+  }
 
-	@Column(nullable = true)
-	private Date updateat;
+  @JsonIgnore
+  @Column(nullable = false)
+  @ColumnDefault("1")
+  private boolean active = true;
 
-	public ChatUser getAuthor() {
-		return author;
-	}
-	public void setAuthor(ChatUser author) {
-		this.author = author;
-	}
-	public Room getRoom() {
-		return room;
-	}
-	public void setRoom(Room room) {
-		this.room = room;
-	}
-	public String getBody() {
-		return body;
-	}
-	public void setBody(String body) {
-		this.body = body;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	@Override
-	public int compareTo(UserMessage o) {
-		if (o==null)return -1;
-		return this.getId().compareTo(o.getId());
-	}
-	@JsonProperty("attachedFilesJson")
-	public ArrayList<String> getAttachedFiles() {
-		ArrayList<String> result = null;
-		ObjectMapper mapper = new ObjectMapper();
-		if (attachedFilesJson==null || attachedFilesJson.length()<1) return new ArrayList<String>();
-		try {
-			result =  mapper.readValue(attachedFilesJson,ArrayList.class);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new ArrayList<String>();
-		}
-		if (result==null) return new ArrayList<String>();
-		return result;
-	}
-	@JsonProperty("attachedFilesJson")
-	public void setAttachedFiles(ArrayList<String> attachedFiles) {
-		if (attachedFiles==null) return ;
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			this.attachedFilesJson = mapper.writeValueAsString(attachedFiles);
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
+  @JsonView(Views.Public.class)
+  @Lob
+  private String attachedFilesJson;
+
+
+  @Column
+  @JsonView(Views.Public.class)
+  private Date date;
+
+  public Date getUpdateat() {
+    return updateat;
+  }
+
+  public void setUpdateat(Date updateat) {
+    this.updateat = updateat;
+  }
+
+  @Column(nullable = true)
+  private Date updateat;
+
+  public ChatUser getAuthor() {
+    return author;
+  }
+  public void setAuthor(ChatUser author) {
+    this.author = author;
+  }
+  public Room getRoom() {
+    return room;
+  }
+  public void setRoom(Room room) {
+    this.room = room;
+  }
+  public String getBody() {
+    return body;
+  }
+  public void setBody(String body) {
+    this.body = body;
+  }
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+  public Date getDate() {
+    return date;
+  }
+  public void setDate(Date date) {
+    this.date = date;
+  }
+  @Override
+  public int compareTo(UserMessage o) {
+    if (o==null)return -1;
+    return this.getId().compareTo(o.getId());
+  }
+  @JsonProperty("attachedFilesJson")
+  public ArrayList<String> getAttachedFiles() {
+    ArrayList<String> result = null;
+    ObjectMapper mapper = new ObjectMapper();
+    if (attachedFilesJson==null || attachedFilesJson.length()<1) return new ArrayList<String>();
+    try {
+      result =  mapper.readValue(attachedFilesJson,ArrayList.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return new ArrayList<String>();
+    }
+    if (result==null) return new ArrayList<String>();
+    return result;
+  }
+  @JsonProperty("attachedFilesJson")
+  public void setAttachedFiles(ArrayList<String> attachedFiles) {
+    if (attachedFiles==null) return ;
+    ObjectMapper mapper = new ObjectMapper();
+    try {
+      this.attachedFilesJson = mapper.writeValueAsString(attachedFiles);
+    } catch (JsonProcessingException e) {
+      e.printStackTrace();
+    }
+  }
+
+
 }
